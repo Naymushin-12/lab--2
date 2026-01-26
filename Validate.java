109942e23a62479747227e3b03307db932e634ee
Siempre <71586429+Naymushin-12@users.noreply.github.com>
2026-01-27 02:55:51 +0500

    Update Validate.java

--- Validate.java
+++ Validate.java
@@ -1,7 +1,7 @@
 
-public class Validate {
+public class Validation {
 
-    public static String getNameErrorMessage(String name) { //проверка корректности на имя человека
+    public static String getNameErrorMessage(String name) { // проверка корректности на имя человека
         if (name == null || name.isEmpty()) {
             return "Имя не может быть пустым";
         }
@@ -14,8 +14,12 @@ public class Validate {
         return null; // Ошибок нет
     }
 
-
-    public static String getNamesErrorMessage(String lastName, String firstName, String patronymic) { //проверка что хотя бы одно из трех имен (фамилия, имя, отчество) задано
+    public static String getNamesErrorMessage(String lastName, String firstName, String patronymic) { // проверка что
+                                                                                                      // хотя бы одно из
+                                                                                                      // трех имен
+                                                                                                      // (фамилия, имя,
+                                                                                                      // отчество)
+                                                                                                      // задано
         if ((lastName == null || lastName.isEmpty()) &&
                 (firstName == null || firstName.isEmpty()) &&
                 (patronymic == null || patronymic.isEmpty())) {
@@ -24,7 +28,7 @@ public class Validate {
         return null; // Ошибок нет
     }
 
-    public static String getHeightErrorMessage(int height) { //проверка корректности роста человека
+    public static String getHeightErrorMessage(int height) { // проверка корректности роста человека
         if (height < 100) {
             return "Рост не может быть меньше 100 см";
         }
@@ -34,16 +38,15 @@ public class Validate {
         return null; // Ошибок нет
     }
 
-
-    public static String getBulletCountErrorMessage(int bullets) { //проверка корректности количества патронов
+    public static String getBulletCountErrorMessage(int bullets) { // проверка корректности количества патронов
         if (bullets < 0) {
             return "Количество патронов не может быть отрицательным";
         }
         return null; // Ошибок нет
     }
 
-
-    public static String getCreatingNameErrorMessage(String firstName) { //специфичная проверка для класса CreatingName - проверяет только имя
+    public static String getCreatingNameErrorMessage(String firstName) { // специфичная проверка для класса CreatingName
+                                                                         // - проверяет только имя
         if (firstName == null || firstName.isEmpty()) {
             return "Личное имя не может быть пустым";
         }
